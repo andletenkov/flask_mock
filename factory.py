f41cb994246f3aca00f37c635f0b60e71ee695a1
andletenkov <58842058+andletenkov@users.noreply.github.com>
2020-07-09 15:24:54 +0300

    Update factory.py

--- factory.py
+++ factory.py
@@ -1,17 +1,27 @@
 import json
+import os
+
 import db
 from json import JSONDecodeError
-from handler import RequestHandler
+from handler import RequestHandler, SkrillRequestHandler
+
+HANDLERS = {
+    # "example": ExamppleRequestHandler
+    # implement your own handler if needed!
+}
 
 
 def get_mock(code):
     """Gets concrete mock client instance"""
     try:
-        with open(f"schemas/{code}.json", "r") as f:
+        with open(
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), "schemas", f"{code}.json"), "r"
+        ) as f:
             schema = json.loads(f.read())
     except FileNotFoundError:
         schema = db.get_schema(code)
     except JSONDecodeError:
         raise Exception("Invalid schema file", 500)
 
-    return RequestHandler(schema)
+    handler = HANDLERS.get(code, RequestHandler)
+    return handler(schema)
